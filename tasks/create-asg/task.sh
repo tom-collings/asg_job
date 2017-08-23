b1b823b645c851975342e251bd25490e443176f3
tom-collings <tom.collings@comcast.net>
2017-08-23 15:20:12 -0600

    adding the creation/update of the group

--- tasks/create-asg/task.sh
+++ tasks/create-asg/task.sh
@@ -14,3 +14,18 @@ fi
 cf auth $CF_USERNAME $CF_PASSWORD
 
 cf target -o $CF_ORG -s $CF_SPACE
+
+secgroupcount=$(cf security-groups | grep $SECURITY_GROUP_NAME | wc -l)
+
+echo $SECURITY_GROUP_VALUE > /tmp/sec.json
+
+if [ $secgroupcount -eq 0 ];
+  then
+    echo "security group not found by name, need to create it"
+    cf create-security-group $SECURITY_GROUP_NAME /tmp/sec.json
+  else
+    echo "security group found by name, need to update it"
+    cf update-security-group $SECURITY_GROUP_NAME /tmp/sec.json
+fi
+
+rm -rf /tmp/sec.json
